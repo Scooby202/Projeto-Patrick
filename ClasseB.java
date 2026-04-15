cf09569b4ee92313dd4cbe6a9e1a145158400d60
Patrick Fernandes <patrick.fernandes@estudante.ufla.br>
2026-04-15 11:21:55 -0300

    Adição das classes A e B

---
+++ ClasseB.java
@@ -0,0 +1,50 @@
+public class ClasseB {
+    
+    private int b1;
+    private float b2;
+
+    public ClasseB(){
+        b1 = 0;
+        b2 = 0;
+    }
+
+    public int getB1(){
+
+        return b1;
+
+    }
+
+    public float getB2(){
+
+        return b2;
+
+    }
+
+    public void setB1(int valor){
+
+        b1 = valor;
+
+    }
+
+    public void setB2(float valor){
+
+        b2 = valor;
+
+    }
+
+    public void MB1(){
+
+
+        System.out.println("MB1");
+
+    }
+
+     public void MB2(){
+
+
+        System.out.println("MB2");
+
+    }
+
+
+}
