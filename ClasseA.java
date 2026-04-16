3660cdee01cb0e49d8c14c4485bd5688efd274e3
Patrick <patrick.fernandes@estudante.ufla.br>
2026-04-16 18:57:13 -0300

    Criação da Classe C

--- ClasseA.java
+++ ClasseA.java
@@ -1,48 +1,52 @@
 public class ClasseA {
-    
+
     private int a1;
     private float a2;
 
-    public ClasseA(){
+    public ClasseA() {
         a1 = 0;
         a2 = 0;
     }
 
-    public int getA1(){
+    public int getA1() {
 
         return a1;
 
     }
 
-    public float getA2(){
+    public float getA2() {
 
         return a2;
 
     }
 
-    public void setA1(int valor){
+    public void setA1(int valor) {
 
         a1 = valor;
 
     }
 
-    public void setA2(float valor){
+    public void setA2(float valor) {
 
         a2 = valor;
 
     }
 
-    public void MA1(){
-
+    public void MA1() {
 
         System.out.println("MA1");
 
     }
 
-     public void MA2(){
-
+    public void MA2() {
 
         System.out.println("MA2");
+
+    }
+
+    public void MA3() {
+
+        System.out.println("Alteração a classe A a partir do clone");
 
     }
 
