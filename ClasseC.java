96e427e5cd41961b216be33d62b51abf8cc56fd9
Patrick <patrick.fernandes@estudante.ufla.br>
2026-04-16 18:54:12 -0300

    Criação da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,25 @@
+public class ClasseC {
+
+  private String c1;
+  private int c2;
+
+  public ClasseC() {
+
+    c1 = "";
+    c2 = 0;
+
+  }
+
+  public void MC1() {
+
+    System.out.println("MC1");
+
+  }
+
+  public void MC2() {
+
+    System.out.println("MC2");
+
+  }
+
+}
