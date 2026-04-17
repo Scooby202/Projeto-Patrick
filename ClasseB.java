9dc818301d9286bd1c414969edc32aab9b27a32f
Patrick <patrick.fernandes@estudante.ufla.br>
2026-04-16 21:04:59 -0300

    Criação método MB3

--- ClasseB.java
+++ ClasseB.java
@@ -1,50 +1,53 @@
 public class ClasseB {
-    
+
     private int b1;
     private float b2;
 
-    public ClasseB(){
+    public ClasseB() {
         b1 = 0;
         b2 = 0;
     }
 
-    public int getB1(){
+    public int getB1() {
 
         return b1;
 
     }
 
-    public float getB2(){
+    public float getB2() {
 
         return b2;
 
     }
 
-    public void setB1(int valor){
+    public void setB1(int valor) {
 
         b1 = valor;
 
     }
 
-    public void setB2(float valor){
+    public void setB2(float valor) {
 
         b2 = valor;
 
     }
 
-    public void MB1(){
-
+    public void MB1() {
 
         System.out.println("MB1");
 
     }
 
-     public void MB2(){
-
+    public void MB2() {
 
         System.out.println("MB2");
 
     }
 
+    public void MB3() {
+
+        System.out.println("MB3");
+
+    }
 
 }
