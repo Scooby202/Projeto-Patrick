27b9f996bddc98acc0a7a91d6fa8a234a54d96af
Patrick <patrick.fernandes@estudante.ufla.br>
2026-04-16 21:03:13 -0300

    Criação ClasseD

---
+++ ClasseD.java
@@ -0,0 +1,23 @@
+public class ClasseD {
+
+  public int d1;
+  public int d2;
+
+  public ClasseD() {
+    d1 = 0;
+    d2 = 0;
+  }
+
+  public void MD1() {
+
+    System.out.println("Olá mundo.");
+
+  }
+
+  public void MD2() {
+
+    System.out.println("Lorem ipsum dolor sit amet");
+
+  }
+
+}
